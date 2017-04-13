07dff917a13dd404b2d9226ad0981928720432e1
vcordero07 <vcordero07@gmail.com>
2017-04-12 23:07:04 -0400

    Added small tweaks to the final code

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
-//-to load the items of the html into an arrary of objects, the shoppingList
-//- be able to add to that list
-//-pass that info in a html type into the site and load to the end of list
-//-be able to check items and uncheck them
-//-be able to delete item from the list
+//-load items of the html into an arrary of objects, the shoppingList
+//-add to that list
+//-render the new item to the list
+//-check items and uncheck them
+//-delete item from the list
 console.clear();
+
+const classes = {
+    SHOPPING_ITEM: '.shopping-item',
+    SHOPPING_ITEM_CHECKED: 'shopping-item__checked',
+    SHOPPING_LIST: '.shopping-list'
+};
 
 let shoppingList;
 
@@ -15,14 +21,18 @@ let addItem = shoppingItem => {
 };
 
 let deleteItem = shoppingItem => {
-    shoppingList.shift({
-        name: shoppingItem
-    })
+    shoppingList = shoppingList.filter((ele, ind) => ele.name !== shoppingItem);
 }
+
+let renderList = () => {
+    const list = getArrayOfItemsComponents();
+    const ele = $(classes.SHOPPING_LIST);
+    ele.html(list);
+};
 
 let getArrayOfItemsComponents = () => {
     return shoppingList.map((ele, ind) => {
-        const shoppingItemClass = ele.check ? 'shopping-item__checked' : null;
+        const shoppingItemClass = ele.check ? classes.SHOPPING_ITEM_CHECKED : null;
         return `<li>
         <span class="shopping-item ${shoppingItemClass}">${ele.name}</span>
         <div class="shopping-item-controls">
@@ -38,56 +48,53 @@ let getArrayOfItemsComponents = () => {
     });
 };
 
-let renderList = () => {
-    const list = getArrayOfItemsComponents();
-    const ele = $('.shopping-list');
-    ele.html(list);
-};
-
-$('#js-shopping-list-form').submit(event => {
-    event.preventDefault();
-    let input = $('#shopping-list-entry');
-    let inputValue = input.val();
-
-    if ((inputValue !== '') && ($.trim(inputValue) !== '')) {
-        addItem(inputValue);
-        renderList();
-    }
-
-    input.val('');
-});
-
-$('body').on('click', '.shopping-item-toggle', event => {
-    //$('body').delegate( '.shopping-item-toggle', 'click', function(event) {
-    //event.stopPropagation();
-    let item = $(event.currentTarget).closest('li').find('.shopping-item').html();
-
-    shoppingList = shoppingList.map((ele, ind) => {
-        return {
-            name: ele.name,
-            check: ele.name === item ? !ele.check : ele.check
-        };
-    });
-    renderList();
-}); //.change();
-
-$('body').on('click', '.shopping-item-delete', event => {
-    let item = $(event.currentTarget).closest('li').find('.shopping-item').html();
-    deleteItem(item);
-    $(event.currentTarget).closest('li').remove();
-});
-
-let retrievePopulatedItems = () => {
-    return $('.shopping-list').find('.shopping-item').map(function(ind, ele) {
+let getPopulatedItems = () => {
+    return $(classes.SHOPPING_LIST).find(classes.SHOPPING_ITEM).map(function(ind, ele) {
         return {
             name: $(ele).html(),
-            check: $(ele).hasClass('shopping-item__checked')
+            check: $(ele).hasClass(classes.SHOPPING_ITEM_CHECKED)
         }
     });
 };
 
+let initializeEvents = () => {
+    $('body').on('click', '.shopping-item-toggle', event => {
+        let item = $(event.currentTarget).closest('li').find(classes.SHOPPING_ITEM).html();
+
+        shoppingList = shoppingList.map((ele, ind) => {
+            return {
+                name: ele.name,
+                check: ele.name === item ? !ele.check : ele.check
+            };
+        });
+        renderList();
+    });
+
+    $('body').on('click', '.shopping-item-delete', event => {
+        let item = $(event.currentTarget).closest('li').find(classes.SHOPPING_ITEM).html();
+        deleteItem(item);
+        renderList();
+    });
+
+    $('#js-shopping-list-form').submit(event => {
+        event.preventDefault();
+        let input = $('#shopping-list-entry');
+        let inputValue = input.val();
+
+        if ((inputValue !== '') && ($.trim(inputValue) !== '')) {
+            addItem(inputValue);
+            renderList();
+        }
+
+        input.val('');
+    });
+
+};
+
+
 let init = () => {
-    shoppingList = retrievePopulatedItems().get();
+    shoppingList = getPopulatedItems().get();
+    initializeEvents();
 }
 
 $(init);
